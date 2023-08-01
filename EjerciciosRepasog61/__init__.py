473e50de9d4286b948b730fdea8f2a53e5479b01
edison100ospina@gmail.com <dlassoc123@gmail.com>
2023-08-01 13:24:46 -0500

    ejercicios 1 y 2

--- EjerciciosRepasog61/__init__.py
+++ EjerciciosRepasog61/__init__.py
@@ -1,1 +1,8 @@
+def calcular_area(radio):
+    area_circulo = 3.14 * (radio**2)
+    print(area_circulo)
 
+
+
+circulo1 = calcular_area(5)
+print(circulo1)
---
+++ EjerciciosRepasog61/ejercicio-1.py
@@ -0,0 +1,4 @@
+nombre = str(input("Ingrese su nombre->"))
+edad = int(input("ingrese su edad"))
+
+print(nombre, edad)
